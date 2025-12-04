a43b5449628fa497fc21acfc3d53e349b9dc2757
LumJiaJun <2500022@sit.singaporetech.edu.sg>
2025-12-04 12:40:00 +0800

    CHanged main.py with chat response

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class chatRequest(BaseModel):
+    prompt: str
+
+class chatResponse(BaseModel):
+    response: str
 
 app = FastAPI()
 
@@ -6,3 +13,6 @@ app = FastAPI()
 def health():
     return {"status": "Ok! This is working woohoo!"}
 
+@app.post("/chat", response_model=chatResponse)
+def chat(request: chatRequest):
+     return chatResponse(response=f"I will get smarter later!")
